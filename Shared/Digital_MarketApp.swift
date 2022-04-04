0965d4ef0687c6e70025fca07f70cb7b60db15f5
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-04-03 22:14:40 -0500

    First commit

---
+++ Shared/Digital_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_MarketApp.swift
+//  Shared
+//
+//  Created by Zoe Schmitt on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Digital_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
